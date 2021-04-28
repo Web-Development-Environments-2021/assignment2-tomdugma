f5ad051bd2426611d177ed1a9f8dd14afb3f7bc4
tomdugma <43905264+tomdugma@users.noreply.github.com>
2021-04-28 16:47:05 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,172 +1,241 @@
-var context;
-var shape = new Object();
-var board;
-var score;
-var pac_color;
-var start_time;
-var time_elapsed;
-var interval;
+let users = [];
+let user_login = false;
+let active_user = null;
 
-$(document).ready(function() {
-	context = canvas.getContext("2d");
-	Start();
+  function afterRegister(){
+	$('#register').hide();
+	$('#register_form').hide();
+	$('#login').show();
+}
+
+function failed_play(){
+	var r = confirm("Please login")
+	if (r == false){
+		window.alert("You need to login to play or Register if you dont have a user")
+	}
+	hideAll();
+	$('#login').show();
+}
+
+function logOut(){
+	user_login = false;
+	active_user = "";
+	afterLogout();
+	stopMusic();
+	cleanBeforeNewGame();
+	hearts = 2; //actually 3
+	gameOver = false;
+	clearInterval(countDownTimer);
+	time_left = max_game_time;
+	$('#container_pre_game').show;
+	$('#container_game').hide;
+	$("#welcome_text").text("Please login if you already have an account. If you don't, what are you waiting for?\nGo\nregister!");
+	$('#welcome').show();
+	$('#nav_login').text('Login')
+  }
+
+  function afterLogout(){
+	$('#welcome').show();
+	$('#game').hide();
+	$('#login').hide();
+	$('#register').hide();
+	$('#game_user').hide();
+	// noinspection JSUnresolvedFunction
+	  $('#alredy_login').hide();
+	$('#register_user').show();
+	$('#login_user').show();
+	// noinspection JSUnresolvedFunction
+	  $('#logout_user').hide();
+	$('#btn_login').show();
+	$('#btn_register').show();
+	$('#btn_play').hide();
+}
+
+$("#register_form").click(function() {
+
+	let valid = true;
+	let firstName = "";
+	let lastName = "";
+	let username = "";
+	var password = "";
+	let email = "";
+	let birthday = "";
+	message = "<br/>";
+	$("#alert_details").empty();
+	$("#alert_register").hide();
+	if( !validName($('#firstName').val(), "first")) //First name
+	{
+		valid = false;
+		message += "Please enter your First name<br/>";
+	}
+	else{
+		firstName = $('#firstName').val();
+	}
+
+	if(!validName($('#lastName').val(), "last")) //Last name
+	{
+		valid = false;
+		message += "Please enter your Last name<br/>";
+	}
+	else
+		{ // noinspection JSUnresolvedFunction
+			lastName = $('#lastName').val();
+		}
+
+	if($('#username').val() === '') //User name
+	{
+		valid = false;
+		message += "Username not valid <br \>";
+	}
+	else { // noinspection JSUnresolvedFunction
+		if(userExists($('#username').val()) ){
+				valid = false;
+				message += "Username already taken. Please choose a new username. <br/>"
+			}
+			else
+				username = $('#username').val();
+	}
+
+	if( !passValid($('#pass').val())){
+		valid = false;
+	}
+	else
+		password = $('#pass').val();
+
+	if( !validEmail($('#email').val()) )
+	{
+		valid = false;
+		message += "Email not valid <br \>";
+	}
+	else
+		email = $('#email').val();
+
+	if($('#datepicker').val() === '')
+	{
+		valid = false;
+		message += "Date not valid <br \>";
+	}
+	else
+		birthday = $('#datepicker').val();
+
+	if (!valid) 
+	{
+		$( "#alert_details" ).empty();
+		$( "#alert_details" ).html(message);
+		$( "#alert_register" ).show();
+	}
+	else
+	{
+		var person = {firstName, lastName, username, password, email, birthday};
+		users.push(person);
+		afterRegister();
+	}
 });
 
-function Start() {
-	board = new Array();
-	score = 0;
-	pac_color = "yellow";
-	var cnt = 100;
-	var food_remain = 50;
-	var pacman_remain = 1;
-	start_time = new Date();
-	for (var i = 0; i < 10; i++) {
-		board[i] = new Array();
-		//put obstacles in (i=3,j=3) and (i=3,j=4) and (i=3,j=5), (i=6,j=1) and (i=6,j=2)
-		for (var j = 0; j < 10; j++) {
-			if (
-				(i == 3 && j == 3) ||
-				(i == 3 && j == 4) ||
-				(i == 3 && j == 5) ||
-				(i == 6 && j == 1) ||
-				(i == 6 && j == 2)
-			) {
-				board[i][j] = 4;
-			} else {
-				var randomNum = Math.random();
-				if (randomNum <= (1.0 * food_remain) / cnt) {
-					food_remain--;
-					board[i][j] = 1;
-				} else if (randomNum < (1.0 * (pacman_remain + food_remain)) / cnt) {
-					shape.i = i;
-					shape.j = j;
-					pacman_remain--;
-					board[i][j] = 2;
-				} else {
-					board[i][j] = 0;
-				}
-				cnt--;
-			}
-		}
+function userExists(userName) {
+
+	for (var i=0; i < users.length; i++) {
+		if(users[i].username === userName)
+		return true
 	}
-	while (food_remain > 0) {
-		var emptyCell = findRandomEmptyCell(board);
-		board[emptyCell[0]][emptyCell[1]] = 1;
-		food_remain--;
-	}
-	keysDown = {};
-	addEventListener(
-		"keydown",
-		function(e) {
-			keysDown[e.keyCode] = true;
-		},
-		false
-	);
-	addEventListener(
-		"keyup",
-		function(e) {
-			keysDown[e.keyCode] = false;
-		},
-		false
-	);
-	interval = setInterval(UpdatePosition, 250);
+	return false;
 }
 
-function findRandomEmptyCell(board) {
-	var i = Math.floor(Math.random() * 9 + 1);
-	var j = Math.floor(Math.random() * 9 + 1);
-	while (board[i][j] != 0) {
-		i = Math.floor(Math.random() * 9 + 1);
-		j = Math.floor(Math.random() * 9 + 1);
-	}
-	return [i, j];
-}
 
-function GetKeyPressed() {
-	if (keysDown[38]) {
-		return 1;
-	}
-	if (keysDown[40]) {
-		return 2;
-	}
-	if (keysDown[37]) {
-		return 3;
-	}
-	if (keysDown[39]) {
-		return 4;
-	}
-}
-
-function Draw() {
-	canvas.width = canvas.width; //clean board
-	lblScore.value = score;
-	lblTime.value = time_elapsed;
-	for (var i = 0; i < 10; i++) {
-		for (var j = 0; j < 10; j++) {
-			var center = new Object();
-			center.x = i * 60 + 30;
-			center.y = j * 60 + 30;
-			if (board[i][j] == 2) {
-				context.beginPath();
-				context.arc(center.x, center.y, 30, 0.15 * Math.PI, 1.85 * Math.PI); // half circle
-				context.lineTo(center.x, center.y);
-				context.fillStyle = pac_color; //color
-				context.fill();
-				context.beginPath();
-				context.arc(center.x + 5, center.y - 15, 5, 0, 2 * Math.PI); // circle
-				context.fillStyle = "black"; //color
-				context.fill();
-			} else if (board[i][j] == 1) {
-				context.beginPath();
-				context.arc(center.x, center.y, 15, 0, 2 * Math.PI); // circle
-				context.fillStyle = "black"; //color
-				context.fill();
-			} else if (board[i][j] == 4) {
-				context.beginPath();
-				context.rect(center.x - 30, center.y - 30, 60, 60);
-				context.fillStyle = "grey"; //color
-				context.fill();
-			}
-		}
-	}
-}
-
-function UpdatePosition() {
-	board[shape.i][shape.j] = 0;
-	var x = GetKeyPressed();
-	if (x == 1) {
-		if (shape.j > 0 && board[shape.i][shape.j - 1] != 4) {
-			shape.j--;
-		}
-	}
-	if (x == 2) {
-		if (shape.j < 9 && board[shape.i][shape.j + 1] != 4) {
-			shape.j++;
-		}
-	}
-	if (x == 3) {
-		if (shape.i > 0 && board[shape.i - 1][shape.j] != 4) {
-			shape.i--;
-		}
-	}
-	if (x == 4) {
-		if (shape.i < 9 && board[shape.i + 1][shape.j] != 4) {
-			shape.i++;
-		}
-	}
-	if (board[shape.i][shape.j] == 1) {
-		score++;
-	}
-	board[shape.i][shape.j] = 2;
-	var currentTime = new Date();
-	time_elapsed = (currentTime - start_time) / 1000;
-	if (score >= 20 && time_elapsed <= 10) {
-		pac_color = "green";
-	}
-	if (score == 50) {
-		window.clearInterval(interval);
-		window.alert("Game completed");
+function validName(name, position) {
+	if (name === "") {
+	return false;
 	} else {
-		Draw();
+	for (let i = 0; i < name.length; i++) {
+		if (!name.charAt(i).match(/[a-z]/i)) {
+		message += "Your " + position + " name may only contain letters<br/>";
+		return false;
+		}
 	}
+	}
+	return true;
 }
+
+function passValid(pass) {
+	let containsNumber = false;
+	let containsAlpha = false;
+	if (pass.length < 8) {
+	message += "Password not valid, must contain at least 8 characters <br >";
+	return false;
+	}
+	for (let i = 0; i < pass.length; i++) {
+	if (pass.charAt(i) >= "0" && pass.charAt(i) <= "9") containsNumber = true;
+	else if (pass.charAt(i).match(/[a-z]/i)) {
+		containsAlpha = true;
+	} else {
+		message += "Password may only contain alpha-numeric characters. <br>";
+		return false;
+	}
+	}
+	if (!(containsAlpha && containsNumber))
+	message +=
+		"Password not valid, must contain numbers AND characters <br >";
+	return containsAlpha && containsNumber;
+}
+
+function validEmail(email) {
+	const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return email.match(re);
+}
+
+$("#sign_in").click(function () {
+	let valid = true;
+	let username = "";
+	let password = "";
+	let message = "";
+	$("#alert_details_login").empty();
+	$("#alert_login").hide();
+
+	if ($("#login_username").val() == "") {
+	valid = false;
+	message += "Username not valid <br >";
+	} else username = $("#login_username").val();
+
+	if ($("#login_password").val() == "") {
+	valid = false;
+	message += "Password not valid <br >";
+	} else password = $("#login_password").val();
+
+	let user = "";
+	for (let i = 0; i < users.length; i++) {
+	if (users[i].username === username)
+		if (users[i].password === password) {
+		user = users[i];
+		break;
+		}
+	}
+
+	if (!valid || user === "") {
+	$("#alert_details_login").empty();
+	$("#alert_details_login").html(message);
+	$("#alert_login").show();
+	message="";
+
+	} else {
+	user_login = true;
+	active_user = user;
+	$("#login").hide();
+	$('#game').show();
+	$("#container_pre_game").show();
+	$("#container_game").hide();
+	$("#ball_numm").val("50");
+	$("#ghost_num").val("3");
+	$("#game_time").val("60");
+	$("#level_check").val("6");
+		$("#btn_login").hide();
+		$("#btn_register").hide();
+	$("#player_name").empty();
+	const user_fullname = active_user.firstName + " " + active_user.lastName;
+	$("#player_name").html(user_fullname);
+	$('#nav_login').text('Logout')
+	$("#btn_play").hide();
+	$("#welcome_text").text("You may play now!");
+	}
+});
+
+
